c2c6d85bd63219d6c18daa096be6da4698396110
Ihor Liubko <liub41k69@gmail.com>
2019-09-20 15:26:29 +0300

    PUG added

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -11,8 +12,9 @@ const PATHS = {
 }
 
 // Pages const for HtmlWebpackPlugin
-const PAGES_DIR = PATHS.src
-// const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html'))
+// const PAGES_DIR = PATHS.src // with HTML files
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 
 module.exports = {
 
@@ -44,22 +46,29 @@ module.exports = {
 	module: {
 		rules: [
 		{
+			test: /\.pug$/,
+			loader: 'pug-loader'
+		},
+		{
 			test: /\.js$/,
 			loader: 'babel-loader',
 			exclude: '/node_modules/'
-		}, {
+		},
+		{
 			test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
 			loader: 'file-loader',
 			options: {
 				name: '[name].[ext]'
 			}
-		}, {
+		},
+		{
 			test: /\.(png|jpg|gif|svg)$/,
 			loader: 'file-loader',
 			options: {
 				name: '[name].[ext]'
 			}
-		}, {
+		},
+		{
 			test: /\.(sa|sc|c)ss$/,
 			use: [
 				'style-loader',
@@ -75,16 +84,23 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: `./css/[name].[hash].css`
 		}),
-		new HtmlWebpackPlugin({
-			hash: false,
-			template: `${PATHS.src}/index.html`,
-			filename: './index.html',
-			// inject: false //if U wanna insert scripts and css manually // visit html-webpack-template
-		}),
+		// uncomment without Pug
+		// new HtmlWebpackPlugin({
+		// 	hash: false,
+		// 	template: `${PATHS.src}/index.html`,
+		// 	filename: './index.html',
+		// 	// inject: false //if U wanna insert scripts and css manually // visit html-webpack-template
+		// }),
 		new CopyWebpackPlugin([
 			{ from: `${PATHS.src}/img`, to: `./img` },
 			{ from: `${PATHS.src}/fonts`, to: `./fonts` },
 			// { from: `${PATHS.src}/static`, to: '' },
 		]),
+
+		// Automatic creation any html pages (Don't forget to RERUN dev server)
+		...PAGES.map(page => new HtmlWebpackPlugin({
+		template: `${PAGES_DIR}/${page}`,
+		filename: `./${page.replace(/\.pug/,'.html')}`
+	 }))
 	],
 }
